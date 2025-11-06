22f2e216d78e121a8c82dd3c161b1457a0ae1e90
dan-acsc <dan-acsc@users.noreply.github.com>
2025-11-06 21:14:59 +0000

    feat: initial release

---
+++ azul_plugin_de4dot/main.py
@@ -0,0 +1,152 @@
+"""Dotnet deobfuscator based on de4dot, used to be part of netalyser."""
+
+import logging
+import os
+import re
+import subprocess  # noqa # nosec: B404
+import tempfile
+import traceback
+
+import pefile
+from azul_runner import (
+    BinaryPlugin,
+    Feature,
+    FeatureType,
+    Job,
+    State,
+    add_settings,
+    cmdline_run,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class De4dot(BinaryPlugin):
+    """De4dot .NET deobfuscator plugin."""
+
+    VERSION = "2025.02.07"  # Note de4dot hasn't been updated in a long time, see https://github.com/0xd4d/de4dot
+    FEATURES = [Feature("obfuscator", desc="Name of obfuscator detected by de4dot", type=FeatureType.String)]
+    SETTINGS = add_settings(
+        filter_data_types={
+            "content": [
+                "executable/windows/dll32",
+                "executable/windows/dll64",
+                "executable/windows/pe",
+                "executable/windows/pe32",
+                "executable/windows/pe64",
+                "executable/dll32",
+                "executable/pe32",
+            ]
+        },
+        filter_max_content_size=(int, 10 * 1024 * 1024),
+        subprocess_timeout=(int, 120),  # Seconds
+    )
+
+    def execute(self, job: Job):
+        """Run the plugin."""
+        data = job.get_data()
+        in_file_path = data.get_filepath()
+        # check if it's really .NET
+        try:
+            with pefile.PE(in_file_path, fast_load=True) as pe:
+                if (
+                    len(pe.OPTIONAL_HEADER.DATA_DIRECTORY) < 15
+                    or pe.OPTIONAL_HEADER.DATA_DIRECTORY[14].VirtualAddress == 0
+                ):
+                    return State(State.Label.OPT_OUT, "Not a .NET assembly")
+        except pefile.PEFormatError:
+            return State(
+                State.Label.ERROR_EXCEPTION, failure_name="Error while parsing PE", message=traceback.format_exc()
+            )
+
+        with tempfile.NamedTemporaryFile() as deob_out_file:
+            de4dot_exe = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), "de4dot_20200609_netcore3", "de4dot.dll"
+            )
+            command = [
+                "dotnet",
+                de4dot_exe,
+                "-f",
+                os.path.join(in_file_path),
+                "-o",
+                os.path.join(deob_out_file.name),
+            ]
+
+            try:
+                res: subprocess.CompletedProcess = subprocess.run(  # nosec: B603
+                    args=command,
+                    stdin=None,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    timeout=self.cfg.subprocess_timeout,
+                    encoding="utf8",
+                )
+            except OSError as e:
+                logger.error("%s: %s for %s" % (e.__class__.__name__, e.args, job.id))
+                return State(
+                    State.Label.ERROR_EXCEPTION,
+                    "%s executing de4dot" % e.__class__.__name__,
+                    message=traceback.format_exc(),
+                )
+            except subprocess.TimeoutExpired:
+                # subprocess.run takes care of terminating the child on timeout
+                logger.error("Timed out running de4dot on %s" % job.id)
+                return State(State.Label.ERROR_EXCEPTION, "Timeout executing de4dot", message=traceback.format_exc())
+
+            if res.returncode != 0:
+                # Handle case that de4dot can detect the file is obfuscated but doesn't know how to reverse it.
+                if "Detected Unknown Obfuscator" in res.stdout:
+                    return State(
+                        State.Label.COMPLETED_EMPTY, message="De4dot does not know how to deobfuscate this file."
+                    )
+                return State(
+                    State.Label.ERROR_EXCEPTION, message=f"Unexpected error occurred when deobfuscating {res.stderr}"
+                )
+
+            deob_file_valid = False
+            deob_out_file.seek(0)
+            deob_file_size = os.stat(deob_out_file.name).st_size
+            if deob_file_size > 0 and deob_file_size < 2 * self.cfg.filter_max_content_size:
+                # Don't add deob file if it's got no content or is twice the size of the original input.
+                deob_file_valid = False
+
+            # Process stdout/results
+            if re.search(r"WARNING: The file isn't a \.NET PE file", res.stdout):
+                self.is_malformed("Malformed dotnet file (may be version > 3.1).")
+                return
+
+            multi_obs = re.search(r"More than one obfuscator detected..(.+)Detected", res.stdout, re.DOTALL)
+            if multi_obs:
+                # multiple detected, and nothing was done probably
+                if deob_file_valid:
+                    self.add_child_with_data_file({"rel_action": "deobfuscated"}, deob_out_file)
+                self.add_feature_values("obfuscator", re.findall(r"\s*([^(]+)\s+\(use: [^)]+\)", multi_obs.group(1)))
+                return State(State.Label.COMPLETED)
+
+            main_obfs = re.search(r"Detected ([^(]+) \(", res.stdout)
+            if not main_obfs:
+                raise AssertionError(
+                    'de4dot executable returned unexpected output - could not find "detected" message'
+                    f"\n -----\n{res.stdout}"
+                )
+
+            # only single deobfuscator used
+            if main_obfs.group(1) == "Unknown Obfuscator":
+                # Ignore the output if an 'Unknown Obfuscator' is detected,
+                # because it reports this for every file
+                return State(State.Label.COMPLETED)
+
+            # OK, it appears to be a valid obfuscator detection
+            if deob_file_valid:
+                self.add_child_with_data_file({"rel_action": "deobfuscated"}, deob_out_file)
+            self.add_feature_values("obfuscator", main_obfs.group(1))
+            return State(State.Label.COMPLETED)
+
+
+def main():
+    """Plugin cmdline entry point."""
+    cmdline_run(plugin=De4dot)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_azul_de4dot.py
@@ -0,0 +1,172 @@
+from azul_runner import FV, Event, JobResult, State, test_template
+
+from azul_plugin_de4dot.main import De4dot
+
+
+class TestExecute(test_template.TestPlugin):
+    PLUGIN_TO_TEST = De4dot
+
+    def test_not_dotnet(self):
+        result = self.do_execution(
+            data_in=[
+                (
+                    "content",
+                    self.load_test_file_bytes(
+                        "702e31ed1537c279459a255460f12f0f2863f973e121cd9194957f4f3e7b0994",
+                        "Benign Windows 32EXE, python library executable python_mcp.exe",
+                    ),
+                )
+            ]
+        )
+        self.assertJobResult(result, JobResult(state=State(State.Label.OPT_OUT, failure_name="Not a .NET assembly")))
+
+    def test_benign_dotnet(self):
+        result = self.do_execution(
+            data_in=[
+                (
+                    "content",
+                    self.load_test_file_bytes(
+                        "9c44230c8e01057143430213fc271fed07a4398b54039980156225d81c184c6c",
+                        "Benign Windows 32EXE, .NET based.",
+                    ),
+                )
+            ]
+        )
+        JobResult(
+            state=State(
+                State.Label.ERROR_EXCEPTION,
+                failure_name="AssertionError",
+            )
+        )
+        self.assertJobResult(
+            result,
+            # No features should be set if no known obfuscator is detected
+            JobResult(state=State(State.Label.COMPLETED_EMPTY)),
+        )
+
+    def test_obfus_dotnet(self):
+        result = self.do_execution(
+            data_in=[
+                (
+                    "content",
+                    self.load_test_file_bytes(
+                        "e05ecc14c86d96d64495434a7e34f35588538ae2e6da20c2dacc4a97001c35df", "Malicious Windows 32EXE."
+                    ),
+                )
+            ]
+        )
+        self.assertJobResult(
+            result,
+            JobResult(
+                state=State(State.Label.COMPLETED),
+                events=[
+                    Event(
+                        entity_type="binary",
+                        entity_id="e05ecc14c86d96d64495434a7e34f35588538ae2e6da20c2dacc4a97001c35df",
+                        features={"obfuscator": [FV(".NET Reactor 4.0")]},
+                    )
+                ],
+            ),
+        )
+
+    def test_confuserex_obfus_dotnet(self):
+        result = self.do_execution(
+            data_in=[
+                (
+                    "content",
+                    self.load_test_file_bytes(
+                        "d30feab410f4b260f6ec56d39969ac8673a585fae4c1308d51136bcd8af0100d",
+                        "Malicious Windows 32EXE, ConfuserEx obfuscation.",
+                    ),
+                )
+            ]
+        )
+        self.assertJobResult(result, JobResult(state=State(State.Label.COMPLETED_EMPTY)))
+
+    def test_dotnet_smart_assembly(self):
+        result = self.do_execution(
+            data_in=[
+                (
+                    "content",
+                    self.load_test_file_bytes(
+                        "f5d82956ceca26f10317df7cfbfacfe99d3854b8aa3fea507e28676f6a6cd9ed",
+                        "Malicious .NET Windows 32EXE, malware family REMCOS",
+                    ),
+                )
+            ]
+        )
+        self.assertJobResult(
+            result,
+            JobResult(
+                state=State(State.Label.COMPLETED),
+                events=[
+                    Event(
+                        entity_type="binary",
+                        entity_id="f5d82956ceca26f10317df7cfbfacfe99d3854b8aa3fea507e28676f6a6cd9ed",
+                        features={"obfuscator": [FV("SmartAssembly 6.9.0.114")]},
+                    )
+                ],
+            ),
+        )
+
+    def test_obfus_dotnet_reactor_eziriz(self):
+        result = self.do_execution(
+            data_in=[
+                (
+                    "content",
+                    self.load_test_file_bytes(
+                        "45dc4518fbf43bf4611446159f72cdbc37641707bb924bd2a52644a3af5bab76",
+                        "Malicious Windows 32EXE, keylogger malware family AgentTesla.",
+                    ),
+                )
+            ]
+        )
+        self.assertJobResult(result, JobResult(state=State(State.Label.COMPLETED_EMPTY)))
+
+    def test_obfus_dotnet_manually_obfuscated(self):
+        result = self.do_execution(
+            data_in=[
+                (
+                    "content",
+                    self.load_test_file_bytes(
+                        "083f741817703fc9a9d53267eb5b2d45f579cca7c922a014e2c5dbaf85f0611d",
+                        "Malicious Windows 32EXE, malware family Njrat",
+                    ),
+                )
+            ]
+        )
+        self.assertJobResult(result, JobResult(state=State(State.Label.COMPLETED_EMPTY)))
+
+    def test_malformed_dotnet_file(self):
+        """Test a run where the file is a mono dotnet file but has content has been modified."""
+        data = self.load_test_file_bytes(
+            "b13049711027802304b0f50291d5557e76113b46c0a2258b919e65d519ace2f2", "Malicious .NET Windows 32EXE."
+        )
+        result = self.do_execution(data_in=[("content", data)])
+        self.assertJobResult(
+            result,
+            JobResult(
+                state=State(State.Label.COMPLETED),
+                events=[
+                    Event(
+                        entity_type="binary",
+                        entity_id="b13049711027802304b0f50291d5557e76113b46c0a2258b919e65d519ace2f2",
+                        features={"malformed": [FV("Malformed dotnet file (may be version > 3.1).")]},
+                    )
+                ],
+            ),
+        )
+
+    def test_unknown_deob(self):
+        """Test a run where the file has been obfuscated with an obfuscator de4dot doesn't recognise."""
+        data = self.load_test_file_bytes(
+            "6ade497b4a45a2c4688ac69fe2ae146c721db3cf8d82df9b5ca40b4614ad62b7",
+            "Malicious .NET Windows 32EXE, malware family AgentTesla.",
+        )
+        result = self.do_execution(data_in=[("content", data)])
+        self.assertJobResult(
+            result,
+            JobResult(
+                state=State(State.Label.COMPLETED_EMPTY, message="De4dot does not know how to deobfuscate this file.")
+            ),
+        )
